eb555181a327f6dfb48331452aa9d6a193b49366
Christopher Mendoza <tiptop1112@gmail.com>
2024-07-18 09:50:57 -0700

    Created variables and function created variables that will represent the users inputs when making a calculation firstNumber and secondNumber represent the number button the user presses and operator represents the operator button that the user presses when making a calculation. the operation function will call an operation function based on user input on HTML calculator

--- main.js
+++ main.js
@@ -11,3 +11,23 @@ function multiply (a,b) {
 function divide (a,b) {
     return a/b;
 }
+// create variables that will represent the firstNumber, operator, and secondNumber of a calculation i.e 1 + 1. 
+//These variables will be used to update calculator display later
+let firstNumber = '';
+let secondNumber = '';
+let operator= '';
+
+
+//create a function called operate which takes two numbers and an operator then calls one of the function operations (add, subtract, multiply, divide) on the numbers
+function operate (firstNumber, operator, secondNumber) {
+    switch(operator) {
+        case "+":
+            return add(firstNumber, secondNumber);
+        case "-":
+            return subtract(firstNumber, secondNumber);
+        case "*":
+            return multiply(firstNumber, secondNumber);
+        case "/":
+            return divide(firstNumber, secondNumber);
+    }
+}
