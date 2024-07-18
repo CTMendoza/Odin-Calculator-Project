b5445565d629ac08878969ecb7b67b2ba2f1a5c0
Christopher Mendoza <tiptop1112@gmail.com>
2024-07-17 20:38:50 -0700

    Created functions made the functions that will do the basic mathematical operations of addition, subtraction, multiplication, and division on a calculator

---
+++ main.js
@@ -0,0 +1,13 @@
+// create functions for all the basic math operators found on a calculator (add, subtract, multiply, divide)
+function add (a,b) {
+    return a+b;
+}
+function subtract (a,b) {
+    return a-b;
+}
+function multiply (a,b) {
+    return a*b;
+}
+function divide (a,b) {
+    return a/b;
+}
